dfb36cd04402ae17c73fdf38276b401efe78a392
Kiki Jiang <itsqiqi@hotmail.com>
2015-11-24 23:25:41 -0800

    scrach

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+	before_action :authenticate_user!
+
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = Comment.new(comment_params)
+		@comment.article = @article
+		@comment.user = current_user
+		@comment.save
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
+end
