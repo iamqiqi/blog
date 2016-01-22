349f283c3f8db742260cca592a163c3e386d4adf
Kiki Jiang <itsqiqi@hotmail.com>
2016-01-21 21:59:21 -0800

    fix layout issues

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,8 +6,4 @@ $(document).ready(function() {
 		$('#edit_bio_link').hide();
 		$('#edit_bio_form').removeClass('hidden');
 	});
-
-	// $('#edit_bio_link').click(function() {
-	// 	$('#edit_bio_link').hide;
-	// });
 });
