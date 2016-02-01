268c700c61f2ca008e86ff33b93c89cf69fe574f
Kiki Jiang <itsqiqi@hotmail.com>
2016-01-31 22:47:25 -0800

    unauthorize handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-
-	include Pundit
-	include PublicActivity::StoreController
-
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	include Pundit
+	include PublicActivity::StoreController
+
+	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
 	protected
 
@@ -14,5 +15,12 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
 		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
 		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+	end
+
+	private
+
+	def user_not_authorized
+		flash[:alert] = "You are not authorized to perform this action."
+		redirect_to(request.referrer || root_path)
 	end
 end
