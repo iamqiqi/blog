c379054a9bf684d90853bff1d34d70e7020e636c
Qiqi Jiang <itsqiqi@hotmail.com>
2016-03-14 05:10:09 +0000

    delete brief

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130234712) do
+ActiveRecord::Schema.define(version: 20160314050715) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
